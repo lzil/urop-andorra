b9d23b03980cba981d1761589c625e0bfdfde051
Liang <zhoul@mit.edu>
2015-10-12 16:48:11 -0400

    merging files

--- authors_to_locations.py
+++ authors_to_locations.py
@@ -2,40 +2,78 @@ import csv
 import matplotlib.pyplot as plt
 
 
-DATA_FILE = "data/tweets.csv"
+DATA_FILE = "data/tweet.csv"
 
 def open_file(filename):
-    return open(filename, "r")
+	return open(filename, "r")
 
 def close_file(f):
-    return f.close()
+	return f.close()
 
 def setup_csv_reader(csvf):
-    return csv.DictReader(csvf)
+	return csv.DictReader(csvf)
 
-def get_author_locations(csv_reader):
-    author_location_dict = { }
-    count = 0
-    for row in csv_reader:
-        count += 1
-        if (count % 250) == 0:
-            print count
-        author_id = row["author"]
-        time      = row["time"]
-        lat       = row["lat"]
-        lng       = row["lng"]
-        if author_location_dict.get(author_id) is None:
-            author_location_dict[author_id] = (((lat, lng), time))
-        else:
-            current_list = list(author_location_dict[author_id])
-            current_list.append(((lat,lng), time))
-            current_list = tuple(current_list)
-            author_location_dict[author_id] = current_list
-    return author_location_dict
+def analyze_tweets(csv_reader):
+	#list of locations
+	tweets_data = { }
+	author_location_dict = { }
+	languages_dict = { }
+	hours_dict = { }
+
+	for count, row in enumerate(csv_reader):
+		if (count % 250) == 0:
+			print count
+		author_id = row["author"]
+		time      = row["time"]
+		lat       = row["lat"]
+		lng       = row["lng"]
+		if author_location_dict.get(author_id) is None:
+			author_location_dict[author_id] = (((lat, lng), time))
+		else:
+			current_list = list(author_location_dict[author_id])
+			current_list.append(((lat,lng), time))
+			current_list = tuple(current_list)
+			author_location_dict[author_id] = current_list
+
+		if row['lang'] in languages_dict:
+			languages_dict[row['lang']] += 1
+		else:
+			languages_dict[row['lang']] = 1
+		hr = row['time'].split(' ')[1].split(':')[0]
+		if hr in hours_dict:
+			hours_dict[hr] += 1
+		else:
+			hours_dict[hr] = 1
+
+	tweets_data['author_locations'] = author_location_dict
+	tweets_data['languages'] = languages_dict
+	tweets_data['hours'] = hours_dict
+		
+	return tweets_data
+
+def hours_to_list(hours_dict):
+	hourList = []
+	for hour, num in hours_dict.iteritems():
+		hourList.append((hour, num))
+	hourList = sorted(hourList, key=lambda x: x[0])
+	return hourList
+
+def langs_to_list(lang_dict):
+	langList = []
+	for lang, num in lang_dict.iteritems():
+		langList.append((lang, num))
+	langList = sorted(langList, key=lambda x: x[1])
+	return langList
+	
+
+
 
 tweets_csv = open_file(DATA_FILE)
 csv_reader = setup_csv_reader(tweets_csv)
 
-print get_author_locations(csv_reader)
+tweets_data = analyze_tweets(csv_reader)
+print hours_to_list(tweets_data['hours'])
+print langs_to_list(tweets_data['languages'])
+
 
 close_file(tweets_csv)
