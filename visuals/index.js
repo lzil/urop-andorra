14c2bb2849ad947be98bf803654b7666c2b84d30
Liang <zhoul@mit.edu>
2015-10-15 18:23:18 -0400

    basic graphs

--- visuals/index.js
+++ visuals/index.js
@@ -12,3 +12,55 @@ $(document).ready(function() {
 
 	})
 })
+
+google.load("visualization", "1", {packages:["corechart", 'line']});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+
+var data = google.visualization.arrayToDataTable([
+  ['Language', 'percent'],
+  ['es',     1891],
+  ['pt',      1394],
+  ['en',  		842],
+  ['fr', 408],
+  ['it', 98],
+  ['nl', 45],
+  ['ro', 37],
+  ['other', 190]
+]);
+
+var options = {
+  title: 'Language Distribution'
+};
+
+var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+chart.draw(data, options);
+}
+
+google.setOnLoadCallback(drawBackgroundColor);
+
+function drawBackgroundColor() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'Calls');
+
+      data.addRows([
+        [0, 94],   [1, 160],  [2, 120],  [3, 127],  [4, 89],  [5, 95],
+        [6, 151],  [7, 143],  [8, 164],  [9, 182],  [10, 182], [11, 234],
+        [12, 234], [13, 244], [14, 234], [15, 262], [16, 311], [17, 250],
+        [18, 293], [19, 341], [20, 379], [21, 368], [22, 287], [23, 185]
+      ]);
+
+      var options = {
+        hAxis: {
+          title: 'Number of Calls'
+        },
+        vAxis: {
+          title: 'Hour'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
