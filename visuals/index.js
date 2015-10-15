d73fcad4d2875dcb32b0d8b144243f02750473f4
Liang <zhoul@mit.edu>
2015-10-15 17:57:53 -0400

    fonts

--- visuals/index.js
+++ visuals/index.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-	$('.navbar-nav li').click(function() {
-		$('.navbar-nav li').removeClass('active');
+	$('.tab').hide();
+	$('#tab-stats').show();
+	$('.tab-links li').click(function() {
+		$('.tab-links li').removeClass('active');
 		$(this).addClass('active');
+		$('.tab').hide();
+		console.log(this)
+		var id = $(this).attr('id')
+		console.log(id)
+		$('#tab-' + id).show();
+
 	})
 })
